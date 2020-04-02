2976a6b3aada4aa45497b9c3d84a75ab78729487
Szymon Rodziewicz <dev.jaca@gmail.com>
2020-04-02 14:28:12 +0200

    Updates

---
+++ precache-manifest.0929285f149f9d4f4c92e03eab2115a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b1a533367f3ac252720df555a2e446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2f1884fe1c15221971",
+    "url": "/static/css/main.68549bac.chunk.css"
+  },
+  {
+    "revision": "d7db4346d5d4410f9d42",
+    "url": "/static/js/2.99572325.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99572325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d2f1884fe1c15221971",
+    "url": "/static/js/main.0fafa9cf.chunk.js"
+  },
+  {
+    "revision": "c42d4d6fbd3aea2f3f33",
+    "url": "/static/js/runtime-main.b5bbeed4.js"
+  },
+  {
+    "revision": "677831e0ea22572b329f130bde9f7d2e",
+    "url": "/static/media/roboto_grid.677831e0.png"
+  }
+]);
